8710c521c74a052a402b537ca5fd0545590da4d6
Marveeen <jmvroqueee@gmail.com>
2022-01-30 11:58:58 +0800

    Homepage added

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Cryptocurrencies
+ = () => {
+  return (
+    <div>
+        Cryptocurrencies
+        
+    </div>
+);
+};
+
+export default Cryptocurrencies
+;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'a8e2c852ffmshcf9b5c4d20824a8p14af2djsn9d16ce30bb5c'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
